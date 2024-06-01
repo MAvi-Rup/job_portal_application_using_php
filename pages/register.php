3bb5e9b9afb6876bde27fe1b3650298d01fe16df
MAvi-Rup <avirupmondal19@gmail.comm>
2024-06-02 04:05:42 +0600

    Done

--- pages/register.php
+++ pages/register.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <?php include '../templates/header.php'; ?>
 
 <main>
-    <h1 style="text-align: center;">Register</h1>
+    <h1 style="text-align: center;">Register Here!</h1>
     <?php if (isset($error)) : ?>
         <p><?php echo $error; ?></p>
     <?php endif; ?>
